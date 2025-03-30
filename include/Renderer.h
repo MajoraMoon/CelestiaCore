3f59defa12553f27295f9f5edfe72cacc1628d1b
MajoraMoon <maxwell.kellwood@tuta.io>
2025-03-30 23:07:58 +0200

    added glm lib

--- include/Renderer.h
+++ include/Renderer.h
@@ -8,6 +8,10 @@
 #include <glad/glad.h>
 #include <SDL3/SDL.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include "Shader.h"
 #include "Resources.h"
 
