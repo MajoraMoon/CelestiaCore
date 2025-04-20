0da823e859d7e8297989bd9b3f3a07267d2c6255
Majora <121319119+MajoraMoon@users.noreply.github.com>
2025-04-21 01:46:29 +0200

    removed borderless mode

--- include/WindowSDLGL.h
+++ include/WindowSDLGL.h
@@ -7,101 +7,72 @@
  * dimensions
  */
 
-namespace Celestia
-{
+namespace Celestia {
 
 // forward declarations
 class EventBus;
 
-class WindowSDLGL
-{
+class WindowSDLGL {
 
-  public:
-    WindowSDLGL(const std::string &title, const std::string &version, EventBus &eventBus,
-                unsigned int initialWidth = 1920, unsigned int initialHeight = 1080);
-    ~WindowSDLGL();
+public:
+  WindowSDLGL(const std::string &title, const std::string &version,
+              EventBus &eventBus, unsigned int initialWidth = 1920,
+              unsigned int initialHeight = 1080);
+  ~WindowSDLGL();
 
-    // inner enum class, so it does not conflict with global on/off values
-    // somewhere else. idk how c++ works honestly, I know object oriented design
-    // but c++ is something else lol
-    enum class FullscreenMode
-    {
-        Windowed,
-        Fullscreen,
-        Borderless
-    };
+  // inner enum class, so it does not conflict with global on/off values
+  // somewhere else. idk how c++ works honestly, I know object oriented design
+  // but c++ is something else lol
+  enum class FullscreenMode { Windowed, Fullscreen };
 
-    enum class VsyncMode
-    {
-        Off = 0,
-        On = 1
-    };
+  enum class VsyncMode { Off = 0, On = 1 };
 
-    void publishCurrentWindowSize();
+  void publishCurrentWindowSize();
 
-    void setFullscreenMode(FullscreenMode mode);
+  void setFullscreenMode(FullscreenMode mode);
 
-    void setVsyncMode(VsyncMode mode);
+  void setVsyncMode(VsyncMode mode);
 
-    SDL_Window *getSDLGLWindow() const
-    {
-        return window;
-    }
-    SDL_GLContext getGLContext() const
-    {
-        return glContext;
-    }
+  SDL_Window *getSDLGLWindow() const { return window; }
+  SDL_GLContext getGLContext() const { return glContext; }
 
-    int getWidth() const
-    {
-        return m_width;
-    }
-    int getHeight() const
-    {
-        return m_height;
-    }
+  int getWidth() const { return m_width; }
+  int getHeight() const { return m_height; }
 
-    FullscreenMode getFullscreenMode() const
-    {
-        return windowState.currentMode;
-    }
+  FullscreenMode getFullscreenMode() const { return windowState.currentMode; }
 
-    VsyncMode getVsyncMode() const
-    {
-        return m_VsyncMode;
-    }
+  VsyncMode getVsyncMode() const { return m_VsyncMode; }
 
-  private:
-    EventBus &eventBus;
-    SDL_Window *window;
-    SDL_GLContext glContext;
+private:
+  EventBus &eventBus;
+  SDL_Window *window;
+  SDL_GLContext glContext;
 
-    int m_width;
-    int m_height;
+  int m_width;
+  int m_height;
 
-    bool m_mouseVisible;
-    bool m_windowIsMaximized;
+  bool m_mouseVisible;
+  bool m_windowIsMaximized;
 
-    const char *videoDriver;
+  const char *videoDriver;
 
-    void handleMouseVisibity(unsigned int width, unsigned int height);
-    void handleWindowResize(unsigned int width, unsigned int height);
-    void handleMaximizeWindow();
+  void handleMouseVisibity(unsigned int width, unsigned int height);
+  void handleWindowResize(unsigned int width, unsigned int height);
+  void handleMaximizeWindow();
 
-    void setupEventSubscriptions();
+  void setupEventSubscriptions();
 
-    struct WindowState
-    {
-        FullscreenMode currentMode = FullscreenMode::Windowed;
-        int prevWidth = 0;
-        int prevHeight = 0;
-        int prevX = 0;
-        int prevY = 0;
-    };
+  struct WindowState {
+    FullscreenMode currentMode = FullscreenMode::Windowed;
+    int prevWidth = 0;
+    int prevHeight = 0;
+    int prevX = 0;
+    int prevY = 0;
+  };
 
-    VsyncMode m_VsyncMode = VsyncMode::Off;
+  VsyncMode m_VsyncMode = VsyncMode::Off;
 
-    WindowState windowState;
+  WindowState windowState;
 };
 
 } // namespace Celestia
--- src/WindowSDLGL.cpp
+++ src/WindowSDLGL.cpp
@@ -4,272 +4,233 @@
 #include "EventSystem.h"
 // clang-format on
 
-namespace Celestia
-{
+namespace Celestia {
 
-WindowSDLGL::WindowSDLGL(const std::string &title, const std::string &version, EventBus &eventBus,
-                         unsigned int initialWidth, unsigned int initialHeight)
-    : eventBus(eventBus), m_width(initialWidth), m_height(initialHeight)
-{
+WindowSDLGL::WindowSDLGL(const std::string &title, const std::string &version,
+                         EventBus &eventBus, unsigned int initialWidth,
+                         unsigned int initialHeight)
+    : eventBus(eventBus), m_width(initialWidth), m_height(initialHeight) {
 
-    // Metadata is new in SDL3, why not using it :)
-    SDL_SetAppMetadata(title.c_str(), version.c_str(), nullptr);
+  // Metadata is new in SDL3, why not using it :)
+  SDL_SetAppMetadata(title.c_str(), version.c_str(), nullptr);
 
-    if (SDL_Init(SDL_INIT_VIDEO) == false)
-    {
-        std::cerr << "Could not initialize SDL: " << SDL_GetError() << std::endl;
-        window = nullptr;
-        glContext = nullptr;
-        return;
-    }
+  if (SDL_Init(SDL_INIT_VIDEO) == false) {
+    std::cerr << "Could not initialize SDL: " << SDL_GetError() << std::endl;
+    window = nullptr;
+    glContext = nullptr;
+    return;
+  }
 
-    // OpenGL-context attribute set up
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+  // OpenGL-context attribute set up
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
-    // instead of creating a window with properties directly, using the more
-    // modular approach here.
-    SDL_PropertiesID props = SDL_CreateProperties();
-    if (props == 0)
-    {
-        std::cerr << "Unable to create properties: " << SDL_GetError() << std::endl;
-        window = nullptr;
-        glContext = nullptr;
-        return;
-    }
+  // instead of creating a window with properties directly, using the more
+  // modular approach here.
+  SDL_PropertiesID props = SDL_CreateProperties();
+  if (props == 0) {
+    std::cerr << "Unable to create properties: " << SDL_GetError() << std::endl;
+    window = nullptr;
+    glContext = nullptr;
+    return;
+  }
 
-    SDL_SetStringProperty(props, SDL_PROP_WINDOW_CREATE_TITLE_STRING, title.c_str());
-    SDL_SetBooleanProperty(props, SDL_PROP_WINDOW_CREATE_RESIZABLE_BOOLEAN, true);
-    SDL_SetNumberProperty(props, SDL_PROP_WINDOW_CREATE_WIDTH_NUMBER, m_width);
-    SDL_SetNumberProperty(props, SDL_PROP_WINDOW_CREATE_HEIGHT_NUMBER, m_height);
-    SDL_SetBooleanProperty(props, SDL_PROP_WINDOW_CREATE_OPENGL_BOOLEAN, true);
-    SDL_SetBooleanProperty(props, SDL_PROP_WINDOW_CREATE_HIGH_PIXEL_DENSITY_BOOLEAN, true);
+  SDL_SetStringProperty(props, SDL_PROP_WINDOW_CREATE_TITLE_STRING,
+                        title.c_str());
+  SDL_SetBooleanProperty(props, SDL_PROP_WINDOW_CREATE_RESIZABLE_BOOLEAN, true);
+  SDL_SetNumberProperty(props, SDL_PROP_WINDOW_CREATE_WIDTH_NUMBER, m_width);
+  SDL_SetNumberProperty(props, SDL_PROP_WINDOW_CREATE_HEIGHT_NUMBER, m_height);
+  SDL_SetBooleanProperty(props, SDL_PROP_WINDOW_CREATE_OPENGL_BOOLEAN, true);
+  SDL_SetBooleanProperty(
+      props, SDL_PROP_WINDOW_CREATE_HIGH_PIXEL_DENSITY_BOOLEAN, true);
 
-    window = SDL_CreateWindowWithProperties(props);
-    if (window == nullptr)
-    {
-        // idk which error log feature is actually better to use
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Could not initiate Window with custom wayland context: %s\n",
-                     SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        glContext = nullptr;
-        return;
-    }
+  window = SDL_CreateWindowWithProperties(props);
+  if (window == nullptr) {
+    // idk which error log feature is actually better to use
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                 "Could not initiate Window with custom wayland context: %s\n",
+                 SDL_GetError());
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    glContext = nullptr;
+    return;
+  }
 
-    SDL_GetWindowSize(window, &m_width, &m_height);
+  SDL_GetWindowSize(window, &m_width, &m_height);
 
-    glContext = SDL_GL_CreateContext(window);
-    if (!glContext)
-    {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Could not create OpenGL context: %s\n", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        glContext = nullptr;
-        return;
-    }
+  glContext = SDL_GL_CreateContext(window);
+  if (!glContext) {
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                 "Could not create OpenGL context: %s\n", SDL_GetError());
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    glContext = nullptr;
+    return;
+  }
 
-    // init glad
-    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress))
-    {
-        std::cerr << "Could not load glad." << std::endl;
-        SDL_GL_DestroyContext(glContext);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        glContext = nullptr;
-        window = nullptr;
-        return;
-    }
-
-    glViewport(0, 0, m_width, m_height);
-
-    if (!SDL_SetWindowRelativeMouseMode(window, true))
-    {
-
-        std::cerr << "Unable to set Mouse to relative Mode: " << SDL_GetError() << std::endl;
-    }
-
-    SDL_GetWindowPosition(window, &windowState.prevX, &windowState.prevY);
-    windowState.prevWidth = m_width;
-    windowState.prevHeight = m_height;
-
-    videoDriver = SDL_GetCurrentVideoDriver();
-
-    // Initialize VSync
-    SDL_GL_SetSwapInterval(static_cast<int>(m_VsyncMode));
-
-    setupEventSubscriptions();
-}
-
-WindowSDLGL::~WindowSDLGL()
-{
-
+  // init glad
+  if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+    std::cerr << "Could not load glad." << std::endl;
     SDL_GL_DestroyContext(glContext);
     SDL_DestroyWindow(window);
     SDL_Quit();
-    SDL_Log("Quitted successfully");
+    glContext = nullptr;
+    window = nullptr;
+    return;
+  }
+
+  glViewport(0, 0, m_width, m_height);
+
+  if (!SDL_SetWindowRelativeMouseMode(window, true)) {
+
+    std::cerr << "Unable to set Mouse to relative Mode: " << SDL_GetError()
+              << std::endl;
+  }
+
+  SDL_GetWindowPosition(window, &windowState.prevX, &windowState.prevY);
+  windowState.prevWidth = m_width;
+  windowState.prevHeight = m_height;
+
+  videoDriver = SDL_GetCurrentVideoDriver();
+
+  // Initialize VSync
+  SDL_GL_SetSwapInterval(static_cast<int>(m_VsyncMode));
+
+  setupEventSubscriptions();
 }
 
-void WindowSDLGL::handleMouseVisibity(unsigned int width, unsigned int height)
-{
+WindowSDLGL::~WindowSDLGL() {
 
-    // invert  boolean values.
-    // mouseEvent says: mouse not visible? then false. mouse visible: then true.
-    bool relativeMode = !m_mouseVisible;
+  SDL_GL_DestroyContext(glContext);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  SDL_Log("Quitted successfully");
+}
 
-    if (!SDL_SetWindowRelativeMouseMode(window, relativeMode))
-    {
-        std::cerr << "Unable to set Mouse to relative Mode: " << SDL_GetError() << std::endl;
+void WindowSDLGL::handleMouseVisibity(unsigned int width, unsigned int height) {
+
+  // invert  boolean values.
+  // mouseEvent says: mouse not visible? then false. mouse visible: then true.
+  bool relativeMode = !m_mouseVisible;
+
+  if (!SDL_SetWindowRelativeMouseMode(window, relativeMode)) {
+    std::cerr << "Unable to set Mouse to relative Mode: " << SDL_GetError()
+              << std::endl;
+  }
+
+  // when entering the mouse mode or camera mode, will be placed at the
+  // center of the window
+  SDL_WarpMouseInWindow(window, width / 2, height / 2);
+}
+
+void WindowSDLGL::handleWindowResize(unsigned int width, unsigned int height) {
+
+  // ensure that the dimensions are set. Sometimes it does not set itself
+  // without telling it specifically
+  this->m_width = width;
+  this->m_height = height;
+  glViewport(0, 0, width, height);
+}
+
+void WindowSDLGL::handleMaximizeWindow() {
+
+  // invert  boolean values again
+  bool shouldRestore = !m_windowIsMaximized;
+
+  if (shouldRestore) {
+    // Restore window before resizing
+    if (!SDL_RestoreWindow(window)) {
+      std::cerr << "Could not restore window properties: " << SDL_GetError()
+                << std::endl;
     }
 
-    // when entering the mouse mode or camera mode, will be placed at the
-    // center of the window
-    SDL_WarpMouseInWindow(window, width / 2, height / 2);
-}
+    SDL_DisplayID displayID = SDL_GetDisplayForWindow(window);
+    SDL_Rect usableBounds;
+    if (!SDL_GetDisplayUsableBounds(displayID, &usableBounds)) {
+      std::cerr << "Could not detect usable desktop area: " << SDL_GetError()
+                << std::endl;
+    } else {
+      int newWidth = usableBounds.w / 2;
+      int newHeight = usableBounds.h / 2;
 
-void WindowSDLGL::handleWindowResize(unsigned int width, unsigned int height)
-{
+      SDL_SetWindowSize(window, newWidth, newHeight);
 
-    // ensure that the dimensions are set. Sometimes it does not set itself
-    // without telling it specifically
-    this->m_width = width;
-    this->m_height = height;
-    glViewport(0, 0, width, height);
-}
-
-void WindowSDLGL::handleMaximizeWindow()
-{
-
-    // invert  boolean values again
-    bool shouldRestore = !m_windowIsMaximized;
-
-    if (shouldRestore)
-    {
-        // Restore window before resizing
-        if (!SDL_RestoreWindow(window))
-        {
-            std::cerr << "Could not restore window properties: " << SDL_GetError() << std::endl;
-        }
-
-        SDL_DisplayID displayID = SDL_GetDisplayForWindow(window);
-        SDL_Rect usableBounds;
-        if (!SDL_GetDisplayUsableBounds(displayID, &usableBounds))
-        {
-            std::cerr << "Could not detect usable desktop area: " << SDL_GetError() << std::endl;
-        }
-        else
-        {
-            int newWidth = usableBounds.w / 2;
-            int newHeight = usableBounds.h / 2;
-
-            SDL_SetWindowSize(window, newWidth, newHeight);
-
-            int posX = usableBounds.x + (usableBounds.w - newWidth) / 2;
-            int posY = usableBounds.y + (usableBounds.h - newHeight) / 2;
-            SDL_SetWindowPosition(window, posX, posY);
-        }
+      int posX = usableBounds.x + (usableBounds.w - newWidth) / 2;
+      int posY = usableBounds.y + (usableBounds.h - newHeight) / 2;
+      SDL_SetWindowPosition(window, posX, posY);
     }
-    else
-    {
-        if (!SDL_MaximizeWindow(window))
-        {
-            std::cerr << "Unable to maximize window: " << SDL_GetError() << std::endl;
-        }
+  } else {
+    if (!SDL_MaximizeWindow(window)) {
+      std::cerr << "Unable to maximize window: " << SDL_GetError() << std::endl;
     }
+  }
 }
 
-void WindowSDLGL::setFullscreenMode(FullscreenMode mode)
-{
-    if (mode == windowState.currentMode)
-        return;
+void WindowSDLGL::setFullscreenMode(FullscreenMode mode) {
+  if (mode == windowState.currentMode)
+    return;
 
-    bool isWayland = videoDriver && (strcmp(videoDriver, "wayland") == 0);
+  bool isWayland = videoDriver && (strcmp(videoDriver, "wayland") == 0);
 
-    switch (mode)
-    {
-    case FullscreenMode::Windowed:
-        SDL_SetWindowFullscreen(window, false);
+  switch (mode) {
+  case FullscreenMode::Windowed:
+    SDL_SetWindowFullscreen(window, false);
 
-        if (isWayland)
-        {
-            SDL_SetWindowSize(window, windowState.prevWidth, windowState.prevHeight);
-            SDL_SetWindowBordered(window, true);
-        }
-        else
-        {
-            SDL_SetWindowBordered(window, true);
-            SDL_SetWindowSize(window, windowState.prevWidth, windowState.prevHeight);
-            SDL_SetWindowPosition(window, windowState.prevX, windowState.prevY);
-        }
-
-        break;
-
-    case FullscreenMode::Fullscreen:
-        if (!isWayland)
-        {
-            SDL_GetWindowPosition(window, &windowState.prevX, &windowState.prevY);
-        }
-        SDL_GetWindowSize(window, &windowState.prevWidth, &windowState.prevHeight);
-        SDL_SetWindowFullscreen(window, true);
-        break;
-
-    case FullscreenMode::Borderless:
-        // wayland needs to be really special like always, so no real borderless for wayland
-        if (isWayland)
-        {
-            SDL_SetWindowFullscreen(window, true);
-        }
-        else
-        {
-            SDL_DisplayID display = SDL_GetPrimaryDisplay();
-            SDL_GetCurrentDisplayMode(display);
-            const SDL_DisplayMode *displayMode = SDL_GetCurrentDisplayMode(display);
-            SDL_GetWindowSize(window, &windowState.prevWidth, &windowState.prevHeight);
-            SDL_GetWindowPosition(window, &windowState.prevX, &windowState.prevY);
-            SDL_SetWindowBordered(window, false);
-            SDL_SetWindowSize(window, displayMode->w, displayMode->h);
-            SDL_SetWindowPosition(window, 0, 0);
-        }
-
-        break;
+    if (isWayland) {
+      SDL_SetWindowSize(window, windowState.prevWidth, windowState.prevHeight);
+      SDL_SetWindowBordered(window, true);
+    } else {
+      SDL_SetWindowBordered(window, true);
+      SDL_SetWindowSize(window, windowState.prevWidth, windowState.prevHeight);
+      SDL_SetWindowPosition(window, windowState.prevX, windowState.prevY);
     }
 
-    windowState.currentMode = mode;
-    publishCurrentWindowSize();
+    break;
+
+  case FullscreenMode::Fullscreen:
+    if (!isWayland) {
+      SDL_GetWindowPosition(window, &windowState.prevX, &windowState.prevY);
+    }
+    SDL_GetWindowSize(window, &windowState.prevWidth, &windowState.prevHeight);
+    SDL_SetWindowFullscreen(window, true);
+    break;
+  }
+
+  windowState.currentMode = mode;
+  publishCurrentWindowSize();
 }
 
-void WindowSDLGL::setVsyncMode(VsyncMode mode)
-{
-    if (mode == m_VsyncMode)
-        return;
-    m_VsyncMode = mode;
-    SDL_GL_SetSwapInterval(static_cast<int>(m_VsyncMode));
+void WindowSDLGL::setVsyncMode(VsyncMode mode) {
+  if (mode == m_VsyncMode)
+    return;
+  m_VsyncMode = mode;
+  SDL_GL_SetSwapInterval(static_cast<int>(m_VsyncMode));
 }
 
-void WindowSDLGL::publishCurrentWindowSize()
-{
-    eventBus.publish(WindowResizeEvent(m_width, m_height));
+void WindowSDLGL::publishCurrentWindowSize() {
+  eventBus.publish(WindowResizeEvent(m_width, m_height));
 }
 
-void WindowSDLGL::setupEventSubscriptions()
-{
-    eventBus.subscribe<WindowResizeEvent>([this](const WindowResizeEvent &ev) {
-        m_width = ev.width;
-        m_height = ev.height;
+void WindowSDLGL::setupEventSubscriptions() {
+  eventBus.subscribe<WindowResizeEvent>([this](const WindowResizeEvent &ev) {
+    m_width = ev.width;
+    m_height = ev.height;
 
-        handleWindowResize(m_width, m_height);
-    });
+    handleWindowResize(m_width, m_height);
+  });
 
-    eventBus.subscribe<MouseVisibilityChanged>([this](const MouseVisibilityChanged &ev) {
+  eventBus.subscribe<MouseVisibilityChanged>(
+      [this](const MouseVisibilityChanged &ev) {
         m_mouseVisible = ev.visible;
         handleMouseVisibity(m_width, m_height);
-    });
+      });
 
-    eventBus.subscribe<WindowMaximizedChanged>([this](const WindowMaximizedChanged &ev) {
+  eventBus.subscribe<WindowMaximizedChanged>(
+      [this](const WindowMaximizedChanged &ev) {
         m_windowIsMaximized = ev.maximized;
         handleMaximizeWindow();
-    });
+      });
 }
 
 } // namespace Celestia
