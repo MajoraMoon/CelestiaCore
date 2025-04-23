8e3dfc10e1956f1bcacc9a7e64d94dcb937ec16e
MajoraMoon <maxwell.kellwood@tuta.io>
2025-04-23 16:17:48 +0200

    better appState naming

--- include/AppState.h
+++ include/AppState.h
@@ -8,7 +8,8 @@
  *
  */
 
-namespace Celestia {
+namespace Celestia
+{
 
 // forward declarations
 class EventBus;
@@ -16,30 +17,51 @@ struct AppState;
 
 // these "global" states have the class+usage naming decleration (to refer where
 // the specific value is effecting which class)
-struct AppState {
-  bool guiManagerGuiVisible = true;
-  bool frameTimerSimulationPaused = false;
+struct AppState
+{
+    struct Gui
+    {
+        bool visible = true;
+    } gui;
 
-  bool windowMouseVisible = false;
-  bool windowMaximized = false;
-  bool windowFullscreen = false;
+    struct Simulation
+    {
+        bool paused = false;
+    } simulation;
 
-  bool celestiaCoreQuit;
-  float cameraMouseSensitivity = 0.1f;
+    struct Window
+    {
+        bool mouseVisible = false;
+        bool maximized = false;
+        bool fullscreen = false;
+    } window;
+
+    struct CelestiaCore
+    {
+        // no default initialization needed because if that is true, the whole program is burned next to the sun
+        // already. I good english speak very gut
+        bool quit;
+    } celestiaCore;
+
+    struct Camera
+    {
+        float mouseSensitivity = 0.150f;
+    } camera;
 };
 
-class StateManager {
+class StateManager
+{
 
-public:
-  StateManager(EventBus &eventBus, AppState &state);
+  public:
+    StateManager(EventBus &eventBus, AppState &state);
 
-  void publishInitialStates();
+    void publishInitialStates();
 
-private:
-  EventBus &eventBus;
-  AppState &state;
+  private:
+    EventBus &eventBus;
+    AppState &state;
 
-  void setupSubscriptions();
+    void setupSubscriptions();
 };
 
 } // namespace Celestia
--- src/AppState.cpp
+++ src/AppState.cpp
@@ -1,96 +1,93 @@
 #include "EventSystem.h"
 #include "pch.h"
 
-namespace Celestia {
-StateManager::StateManager(EventBus &eb, AppState &s) : eventBus(eb), state(s) {
-  setupSubscriptions();
+namespace Celestia
+{
+StateManager::StateManager(EventBus &eb, AppState &s) : eventBus(eb), state(s)
+{
+    setupSubscriptions();
 }
 
-void StateManager::setupSubscriptions() {
+void StateManager::setupSubscriptions()
+{
 
-  /**
-   *
-   * These values should be the same for every class accessing them. So it is an
-   * implementation of a global variable, but only indirectly.
-   *
-   * These are subscriptions, which trigger to change a  boolean value and then
-   * publish the value to another specific event which can be accessed to use
-   * the boolean value.
-   *
-   * idk if I explain it well, but these functions should not be that
-   * complicated to understand if you know how the basic idea of this BusEvent
-   * works
-   */
+    /**
+     *
+     * These values should be the same for every class accessing them. So it is an
+     * implementation of a global variable, but only indirectly.
+     *
+     * These are subscriptions, which trigger to change a  boolean value and then
+     * publish the value to another specific event which can be accessed to use
+     * the boolean value.
+     *
+     * idk if I explain it well, but these functions should not be that
+     * complicated to understand if you know how the basic idea of this BusEvent
+     * works
+     */
 
-  /* ----------- Events emited (published) by keyboard/mouse input----------- */
+    /* ----------- Events emited (published) by keyboard/mouse input ----------- */
 
-  // Mouse visibility
-  eventBus.subscribe<ToggleMouseVisibilityEvent>(
-      [this](const ToggleMouseVisibilityEvent &) {
-        state.windowMouseVisible = !state.windowMouseVisible;
-        eventBus.publish(MouseVisibilityChanged{state.windowMouseVisible});
-      });
+    // Mouse visibility
+    eventBus.subscribe<ToggleMouseVisibilityEvent>([this](const ToggleMouseVisibilityEvent &) {
+        state.window.mouseVisible = !state.window.mouseVisible;
+        eventBus.publish(MouseVisibilityChanged{state.window.mouseVisible});
+    });
 
-  // GUI visibility
-  eventBus.subscribe<ToggleGuiVisibilityEvent>(
-      [this](const ToggleGuiVisibilityEvent &) {
-        state.guiManagerGuiVisible = !state.guiManagerGuiVisible;
-        eventBus.publish(GuiVisibilityChanged{state.guiManagerGuiVisible});
-      });
+    // Window state
+    eventBus.subscribe<ToggleWindowMaximizedEvent>([this](const ToggleWindowMaximizedEvent &) {
+        state.window.maximized = !state.window.maximized;
+        eventBus.publish(WindowMaximizedChanged{state.window.maximized});
+    });
 
-  // Pause state
-  eventBus.subscribe<TogglePauseEvent>([this](const TogglePauseEvent &) {
-    state.frameTimerSimulationPaused = !state.frameTimerSimulationPaused;
-    eventBus.publish(SimulationPausedChanged{state.frameTimerSimulationPaused});
-  });
+    // GUI visibility
+    eventBus.subscribe<ToggleGuiVisibilityEvent>([this](const ToggleGuiVisibilityEvent &) {
+        state.gui.visible = !state.gui.visible;
+        eventBus.publish(GuiVisibilityChanged{state.gui.visible});
+    });
 
-  // Window state
-  eventBus.subscribe<ToggleWindowMaximizedEvent>(
-      [this](const ToggleWindowMaximizedEvent &) {
-        state.windowMaximized = !state.windowMaximized;
-        eventBus.publish(WindowMaximizedChanged{state.windowMaximized});
-      });
+    // Pause state
+    eventBus.subscribe<TogglePauseEvent>([this](const TogglePauseEvent &) {
+        state.simulation.paused = !state.simulation.paused;
+        eventBus.publish(SimulationPausedChanged{state.simulation.paused});
+    });
 
-  // the quitEvent does not have multiple bool changes because when the main
-  // loop is stopped, the whole simulation and therefore the window disappears.
-  eventBus.subscribe<QuitEvent>([this](const QuitEvent &) {
-    state.celestiaCoreQuit = true;
-    eventBus.publish(CelestiaCoreQuitChanged{state.celestiaCoreQuit});
-  });
+    // the quitEvent does not have multiple bool changes because when the main
+    // loop is stopped, the whole simulation and therefore the window disappears.
+    eventBus.subscribe<QuitEvent>([this](const QuitEvent &) {
+        state.celestiaCore.quit = true;
+        eventBus.publish(CelestiaCoreQuitChanged{state.celestiaCore.quit});
+    });
 
-  /* -------------------- Events emited (published) by gui input
-   * ------------------ */
+    /* ---------------- Events emited (published) by gui input -------------- */
 
-  // Mouse sensitivity
-  eventBus.subscribe<SetMouseSensitivityEvent>(
-      [this](const SetMouseSensitivityEvent &ev) {
+    // Mouse sensitivity
+    eventBus.subscribe<SetMouseSensitivityEvent>([this](const SetMouseSensitivityEvent &ev) {
         // no redundant updates for this float value. I might forgot it
         // everywhere else lol
         // So an if-statement costs more than assigning a new value each frame,
         // but it also triggers the eventBus publish function.
-        if (state.cameraMouseSensitivity != ev.sensitivity) {
-          state.cameraMouseSensitivity = ev.sensitivity;
-          eventBus.publish(MouseSensitivityChanged{ev.sensitivity});
-        }
-      });
 
-  // Fullscreen mode
-  eventBus.subscribe<SetFullscreenModeEvent>(
-      [this](const SetFullscreenModeEvent &ev) {
-        state.windowFullscreen = ev.fullscreen;
-        eventBus.publish(WindowFullscreenChanged{ev.fullscreen});
-      });
+        state.camera.mouseSensitivity = ev.sensitivity;
+        eventBus.publish(MouseSensitivityChanged{state.camera.mouseSensitivity});
+    });
+
+    // Fullscreen mode
+    eventBus.subscribe<SetFullscreenModeEvent>([this](const SetFullscreenModeEvent &ev) {
+        state.window.fullscreen = ev.fullscreen;
+        eventBus.publish(WindowFullscreenChanged{state.window.fullscreen});
+    });
 }
 
-void StateManager::publishInitialStates() {
+void StateManager::publishInitialStates()
+{
 
-  // Broadcast initial state to all listeners
-  eventBus.publish(MouseVisibilityChanged{state.windowMouseVisible});
-  eventBus.publish(GuiVisibilityChanged{state.guiManagerGuiVisible});
-  eventBus.publish(SimulationPausedChanged{state.frameTimerSimulationPaused});
-  eventBus.publish(WindowMaximizedChanged{state.windowMaximized});
-  eventBus.publish(MouseSensitivityChanged{state.cameraMouseSensitivity});
-  eventBus.publish(WindowFullscreenChanged{state.windowFullscreen});
+    // Broadcast initial state to all listeners
+    eventBus.publish(MouseVisibilityChanged{state.window.mouseVisible});
+    eventBus.publish(GuiVisibilityChanged{state.gui.visible});
+    eventBus.publish(SimulationPausedChanged{state.simulation.paused});
+    eventBus.publish(WindowMaximizedChanged{state.window.maximized});
+    eventBus.publish(MouseSensitivityChanged{state.camera.mouseSensitivity});
+    eventBus.publish(WindowFullscreenChanged{state.window.fullscreen});
 }
 
 } // namespace Celestia
