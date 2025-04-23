a7d02c07861647dbf163f2bcb9647d101bea6da8
Majora <121319119+MajoraMoon@users.noreply.github.com>
2025-04-24 00:41:13 +0200

    starting changing windwow resize event

--- include/AppState.h
+++ include/AppState.h
@@ -8,8 +8,7 @@
  *
  */
 
-namespace Celestia
-{
+namespace Celestia {
 
 // forward declarations
 class EventBus;
@@ -17,52 +16,49 @@ struct AppState;
 
 // these "global" states have the class+usage naming decleration (to refer where
 // the specific value is effecting which class)
-struct AppState
-{
-    struct Gui
-    {
-        bool visible = true;
-    } gui;
+struct AppState {
+  struct Gui {
+    bool visible = true;
+  } gui;
 
-    struct Simulation
-    {
-        bool paused = false;
-    } simulation;
+  struct Simulation {
+    bool paused = false;
+  } simulation;
 
-    struct Window
-    {
-        bool mouseVisible = false;
-        bool maximized = false;
-        bool fullscreen = false;
-        bool vsync = false;
-    } window;
+  struct Window {
+    bool mouseVisible = false;
+    bool maximized = false;
+    bool fullscreen = false;
+    bool vsync = false;
 
-    struct CelestiaCore
-    {
-        // no default initialization needed because if that is true, the whole program is burned next to the sun
-        // already. I good english speak very gut
-        bool quit;
-    } celestiaCore;
+    uint32_t width = 1920;
+    uint32_t height = 1080;
 
-    struct Camera
-    {
-        float mouseSensitivity = 0.150f;
-    } camera;
+  } window;
+
+  struct CelestiaCore {
+    // no default initialization needed because if that is true, the whole
+    // program is burned next to the sun already. I good english speak very gut
+    bool quit;
+  } celestiaCore;
+
+  struct Camera {
+    float mouseSensitivity = 0.150f;
+  } camera;
 };
 
-class StateManager
-{
+class StateManager {
 
-  public:
-    StateManager(EventBus &eventBus, AppState &state);
+public:
+  StateManager(EventBus &eventBus, AppState &state);
 
-    void publishInitialStates();
+  void publishInitialStates();
 
-  private:
-    EventBus &eventBus;
-    AppState &state;
+private:
+  EventBus &eventBus;
+  AppState &state;
 
-    void setupSubscriptions();
+  void setupSubscriptions();
 };
 
 } // namespace Celestia
